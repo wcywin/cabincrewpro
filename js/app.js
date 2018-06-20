4b4b9bbb188c2f7aaa6f8786dae5a3f6dd2c9993
Wojtek <C:\Users\Wojtek i Marta\Desktop\IT\The BAT!\Gowny katalog poczty programu The Bat!>
2018-06-20 18:22:32 +0200

    test app complete with media queries

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-const support = document.getElementsByClassName('offerings');
+var mainHeading = document.getElementById("mainH1");
+var mainHeadingTwo = document.getElementById("mainH12");
+var mainSlider = document.getElementsByClassName('main__heading');
+//
+// mainHeading.textConten = "Chcesz latać po świecie?";
+// mainHeadingTwo.textContent = "Dowiedz się, które linie lotnicze są dla Ciebie!";
 
-document.addEventListener('resize', function() {
-
-    if (window.width <= 1000) {
-        support.classList.remove('offerings__icon-box');
-    }
-
-
-});
+// setTimeout(function () {
+//     mainHeading.textContent = "Chcesz latać po świecie?";
+//     mainHeadingTwo.innerHTML = "Dowiedz sie, gdzie <span>warto</span> aplikować!!";
+// }, 3000);
+//
+// setTimeout(function () {
+//     document.getElementById('mainH1').textContent = "Chcesz zostać Stewardem / Stewardessą?";
+//     document.getElementById('mainH12').textContent = "- Przygotujemy Cię do rekrutacji!";
+// }, 3000);
